c77cbd3925f73c0c6ab70b6def350d85c3540b52
markklimenko <193284037454>
2023-03-15 17:03:40 +0300

    WIP: Implement settings loader - 2

--- src/main/kotlin/com/go/feature/service/SettingsLoaderService.kt
+++ src/main/kotlin/com/go/feature/service/SettingsLoaderService.kt
@@ -16,9 +16,11 @@ import com.go.feature.persistence.repository.NamespaceRepository
 import com.go.feature.service.dto.loader.LoadedSettings
 import com.go.feature.util.randomId
 import kotlinx.coroutines.flow.collect
+import mu.KLogging
+import org.apache.commons.codec.digest.DigestUtils
 import org.springframework.stereotype.Service
-import java.nio.file.Files
-import java.nio.file.Paths
+import org.springframework.transaction.annotation.Transactional
+import java.io.File
 
 @Service
 class SettingsLoaderService(
@@ -32,80 +34,114 @@ class SettingsLoaderService(
     val featureConverter: FeatureConverter,
 ) {
 
-    //TODO: load all directory
     suspend fun loadSettings() {
-        // TODO: do for each file
-        // TODO: transactional for each file
         if (applicationProperties.loader.enabled) {
-            // TODO: read bytes nonblocking
-            val settings: LoadedSettings =
-                objectMapper.readValue(Files.readAllBytes(Paths.get(applicationProperties.loader.location)))
+            val files: Array<File>? = File(applicationProperties.loader.location)
+                .listFiles { _: File, name: String -> name.endsWith(SETTINGS_FILE_TYPE) }
 
-            val configHash: String = "TODO1" // TODO: implement
+            if (files == null) {
+                logger.warn(
+                    "${LOG_PREFIX} Settings location not found; " +
+                            "location=${applicationProperties.loader.location}"
+                )
+                return
+            }
 
-            val namespace: Namespace = namespaceRepository.findByName(settings.namespace.name)
-                ?: namespaceRepository.save(namespaceConverter.create(settings.namespace))
+            if (files.isEmpty()) {
+                logger.warn(
+                    "${LOG_PREFIX} Settings location is empty; " +
+                            "fileType=${SETTINGS_FILE_TYPE}, " +
+                            "location=${applicationProperties.loader.location}"
+                )
+                return
+            }
 
-            val indexVersion: IndexVersion? = indexVersionRepository.findByNamespace(namespace.id)
-
-            // update configs
-            if (indexVersion == null || indexVersion.indexVersion != configHash) {
-                // clean old configs
-                filterRepository.deleteAllByNamespace(namespace.id)
-                featureRepository.deleteAllByNamespace(namespace.id)
-
-                // add new configs
-                val filters: List<Filter> = settings.filters.map {
-                    Filter(
-                        id = randomId(),
-                        name = it.name,
-                        namespace = namespace.id,
-                        parameter = it.parameter,
-                        operator = it.operator,
-                        description = it.description
-                    )
-                }
-                filterRepository.saveAll(filters).collect()
-
-                val nameToFilterMap: Map<String, Filter> = filters.associateBy { it.name }
-
-                val features: List<Feature> = settings.features.map {
-                    val featureFilters: List<Feature.Filter> = it.filters.map { filter ->
-                        Feature.Filter(
-                            id = nameToFilterMap[filter.name]?.id
-                                ?: throw IllegalArgumentException("No filter with name=${filter.name}"),
-                            value = filter.value
-                        )
-                    }
-
-                    Feature(
-                        id = randomId(),
-                        name = it.name,
-                        namespace = namespace.id,
-                        filters = objectMapper.writeValueAsString(featureFilters),
-                        status = featureConverter.convertStatus(it.status),
-                        description = it.description
-                    )
-                }
-                featureRepository.saveAll(features).collect()
-
-                // update index version
-                if (indexVersion != null) {
-                    indexVersionRepository.save(
-                        indexVersion.copy(indexVersion = configHash)
-                    )
-                } else {
-                    indexVersionRepository.save(
-                        IndexVersion(
-                            id = randomId(),
-                            namespace = namespace.id,
-                            indexVersion = configHash
-                        )
-                    )
-                }
-            } else {
-                // do nothing, configs are already updated
+            files.forEach {
+                loadSetting(it)
             }
         }
+    }
+
+    @Transactional(rollbackFor = [Exception::class])
+    suspend fun loadSetting(file: File) {
+        val fileByteArray: ByteArray = file.readBytes()
+        val configHash: String = DigestUtils.md5Hex(fileByteArray)
+
+        val settings: LoadedSettings = objectMapper.readValue(fileByteArray)
+
+        val namespace: Namespace = namespaceRepository.findByName(settings.namespace.name)
+            ?: namespaceRepository.save(namespaceConverter.create(settings.namespace))
+
+        logger.info("${LOG_PREFIX} Prepare settings for namespace ${namespace.name}")
+
+        val indexVersion: IndexVersion? = indexVersionRepository.findByNamespace(namespace.id)
+
+        // update configs
+        if (indexVersion == null || indexVersion.indexVersion != configHash) {
+            logger.info("${LOG_PREFIX} Start settings loading for namespace ${namespace.name}")
+
+            // clean old configs
+            filterRepository.deleteAllByNamespace(namespace.id)
+            featureRepository.deleteAllByNamespace(namespace.id)
+
+            // add new configs
+            val filters: List<Filter> = settings.filters.map {
+                Filter(
+                    id = randomId(),
+                    name = it.name,
+                    namespace = namespace.id,
+                    parameter = it.parameter,
+                    operator = it.operator,
+                    description = it.description
+                )
+            }
+            filterRepository.saveAll(filters).collect()
+
+            val nameToFilterMap: Map<String, Filter> = filters.associateBy { it.name }
+
+            val features: List<Feature> = settings.features.map {
+                val featureFilters: List<Feature.Filter> = it.filters.map { filter ->
+                    Feature.Filter(
+                        id = nameToFilterMap[filter.name]?.id
+                            ?: throw IllegalArgumentException("${LOG_PREFIX} No filter with name=${filter.name}"),
+                        value = filter.value
+                    )
+                }
+
+                Feature(
+                    id = randomId(),
+                    name = it.name,
+                    namespace = namespace.id,
+                    filters = objectMapper.writeValueAsString(featureFilters),
+                    status = featureConverter.convertStatus(it.status),
+                    description = it.description
+                )
+            }
+            featureRepository.saveAll(features).collect()
+
+            // update index version
+            if (indexVersion != null) {
+                indexVersionRepository.save(
+                    indexVersion.copy(indexVersion = configHash)
+                )
+            } else {
+                indexVersionRepository.save(
+                    IndexVersion(
+                        id = randomId(),
+                        namespace = namespace.id,
+                        indexVersion = configHash
+                    )
+                )
+            }
+            logger.info("${LOG_PREFIX} Finish settings loading for namespace ${namespace.name}")
+        } else {
+            logger.info("${LOG_PREFIX} Settings are up to date for namespace ${namespace.name}")
+        }
+    }
+
+    private companion object : KLogging() {
+        const val LOG_PREFIX = "SETTINGS_LOADER:"
+
+        const val SETTINGS_FILE_TYPE = ".json"
     }
 }
