39b29c6eea339d901a296967d39be94b24d1dc1f
markklimenko <mark.klimenko.w@gmail.com>
2023-06-30 00:48:33 +0300

    Implement git content provider - test (in process)

--- src/main/kotlin/com/go/feature/component/content/provider/GitContentProvider.kt
+++ src/main/kotlin/com/go/feature/component/content/provider/GitContentProvider.kt
@@ -2,17 +2,39 @@ package com.go.feature.component.content.provider
 
 import mu.KLogging
 import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.errors.RepositoryNotFoundException
+import org.eclipse.jgit.internal.storage.file.FileRepository
+import org.eclipse.jgit.transport.URIish
 import java.io.File
 
 // TODO: implement git loader
 // git show
 class GitContentProvider : ContentProvider {
     override fun getContent(location: String, fileType: String): List<ByteArray> {
-        Git.cloneRepository()
-            .setURI("https://github.com/MarkKlimenko/go-feature.git")
-            .setDirectory(File("tmp/setting/git"))
-            .setBranch("settings")
-            .call()
+
+        var git: Git? = null
+
+        // try to create and update repository
+        try {
+            git = Git.open(File("tmp/setting/git"))
+            git.pull().call()
+            // TODO: update here
+        } catch (e: RepositoryNotFoundException) {
+            logger.debug("Local repository not found")
+        }
+
+        if(git == null) {
+            git = Git.cloneRepository()
+                .setURI("https://github.com/MarkKlimenko/go-feature.git")
+                .setDirectory(File("tmp/setting/git"))
+                .setBranch("feature/implement_git_settings_loader")
+                .setDepth(1)
+                .call()
+        }
+
+
+        //todo: get files
+
 
         TODO("Not yet implemented")
     }
