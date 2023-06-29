1a92eb8e07e27fb7371b16041722a6b1b5342f41
markklimenko <mark.klimenko.w@gmail.com>
2023-06-29 23:29:28 +0300

    Implement git content provider (in process)

--- src/main/kotlin/com/go/feature/component/content/provider/GitContentProvider.kt
+++ src/main/kotlin/com/go/feature/component/content/provider/GitContentProvider.kt
@@ -1,11 +1,19 @@
 package com.go.feature.component.content.provider
 
 import mu.KLogging
+import org.eclipse.jgit.api.Git
+import java.io.File
 
 // TODO: implement git loader
 // git show
 class GitContentProvider : ContentProvider {
     override fun getContent(location: String, fileType: String): List<ByteArray> {
+        Git.cloneRepository()
+            .setURI("https://github.com/eclipse/jgit.git")
+            .setDirectory(File("/data/setting/git"))
+            .setBranch("settings")
+            .call();
+
         TODO("Not yet implemented")
     }
 
